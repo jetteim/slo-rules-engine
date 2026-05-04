a6091616586409a054f08f298f734e2af104bf82
Maxim Lee <jetteim@gmail.com>
2026-05-04 10:10:04 +0100

    feat: validate datadog live apply payloads

--- test/all_test.rb
+++ test/all_test.rb
@@ -2,6 +2,7 @@
 
 require_relative 'forbidden_terms_test'
 require_relative 'cli_test'
+require_relative 'rules_ctl_test'
 require_relative 'dsl_test'
 require_relative 'validation_test'
 require_relative 'providers_test'
